fcca863456d35d3e60b1ded487c8d81b26555348
WESLEY LEONCIO <wesley.leoncio.izi@gmail.com>
2022-03-28 17:26:38 -0300

    Atualização

---
+++ src/models/Produto.java
@@ -0,0 +1,43 @@
+package models;
+
+public class Produto {
+    private String nome;
+    private Double espaco;
+    private Double valor;
+
+    public Produto(String nome, Double espaco, Double valor) {
+        this.nome = nome;
+        this.espaco = espaco;
+        this.valor = valor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getEspaco() {
+        return espaco;
+    }
+
+    public void setEspaco(Double espaco) {
+        this.espaco = espaco;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "nome:" + nome + ", espaco:" + espaco +
+                ", valor:" + valor;
+    }
+}
